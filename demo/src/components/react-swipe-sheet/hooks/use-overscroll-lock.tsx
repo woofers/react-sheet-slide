05deefdda3b61923f8c43d743d11929208b99672
Jaxson Van Doorn <woofers1tt@gmail.com>
2022-05-20 20:04:59 -0700

    Add scroll hooks

---
+++ demo/src/components/react-swipe-sheet/hooks/use-overscroll-lock.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useRef } from 'react'
+
+const useOverscrollLock = ({ enabled }: { enabled?: boolean }) => {
+  const ref = useRef<HTMLDivElement | null>(null)
+  useEffect(() => {
+    const elem = ref.current
+    if (!elem) return
+    const preventScrolling = (e: Event) => {
+      if (false) {
+        e.preventDefault()
+      }
+    }
+    const preventSafariOverscroll = (e: Event) => {
+      if (typeof window === 'undefined') return
+      if (elem.scrollTop < 0) {
+        window.requestAnimationFrame(() => {
+          elem.style.overflow = 'hidden'
+          elem.scrollTop = 0
+          elem.style.removeProperty('overflow')
+        })
+        e.preventDefault()
+      }
+    }
+    if (enabled) {
+      elem.addEventListener('scroll', preventScrolling)
+      elem.addEventListener('touchmove', preventScrolling)
+      elem.addEventListener('touchstart', preventSafariOverscroll)
+    }
+    return () => {
+      if (!enabled) return
+      elem.removeEventListener('scroll', preventScrolling)
+      elem.removeEventListener('touchmove', preventScrolling)
+      elem.removeEventListener('touchstart', preventSafariOverscroll)
+    }
+  }, [enabled, ref])
+  return ref
+}
+
+export default useOverscrollLock
--- demo/src/components/react-swipe-sheet/sheet.tsx
+++ demo/src/components/react-swipe-sheet/sheet.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useOverscrollLock, useScrollLock } from './hooks'
 import classes from './classnames'
 import styles from './sheet.module.css'
 
@@ -9,12 +10,15 @@ type SheetProps = {
 }
 
 const Sheet: React.FC<SheetProps> = ({ children }) => {
+  const scroll = useOverscrollLock({ enabled: true })
+  useScrollLock({ enabled: true, targetRef: scroll })
+
   return (
     <div className={cx('root')}>
       <div className={cx('backdrop', 'stack')}></div>
       <div className={cx('modal', 'stack')}>
         <div className={cx('header')}></div>
-        <div className={cx('scroll')}>
+        <div className={cx('scroll')} ref={scroll}>
           <div className={cx('content')}>{children}</div>
         </div>
         <div className={cx('footer')}></div>
