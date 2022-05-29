38eab9dd0d91ddfd1a3800d93b33a02bad4b7357
Jaxson Van Doorn <woofers1tt@gmail.com>
2022-05-29 12:47:19 -0700

    Remove from modal

--- src/sheet.tsx
+++ src/sheet.tsx
@@ -240,9 +240,9 @@ const BaseSheet = forwardRef<HTMLDivElement, InteralSheetProps>(
       }
     }, [onClose])
     useEffect(() => {
-      if (typeof window === 'undefined' || useModal) return
+      if (typeof window === 'undefined') return
       const className = cx('sheet-open')
-      document.body.classList.add(className)
+      if (!useModal) document.body.classList.add(className)
       return () => {
         document.body.classList.remove(className)
       }
