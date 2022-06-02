995c6f2928ded3783fc366ceebb3fadadce828ba
woofers <woofers1tt@gmail.com>
2022-06-02 03:14:50 +0000

    Updates

---
+++ _next/static/h0lUU0siDtn2VHbUvnB2P/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/258-2bd698e13fec28c3.js","static/chunks/pages/index-c587eeadaf17a3a8.js"],"/_error":["static/chunks/pages/_error-6e6353f79838677c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
--- _next/static/t7hcsO1orfdLCbrv4sFL4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/258-2bd698e13fec28c3.js","static/chunks/pages/index-a0ee7d84da839e5f.js"],"/_error":["static/chunks/pages/_error-6e6353f79838677c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
