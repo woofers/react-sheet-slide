46b1c1a6f271632657a1c23297d0b2f2a4886494
woofers <woofers1tt@gmail.com>
2022-06-12 00:49:22 +0000

    Updates

---
+++ _next/static/LCjEGSdl8i-rdNMTKpg_h/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/770-6c0e9026660adedb.js","static/chunks/pages/index-5c187c304b2ddab0.js"],"/_error":["static/chunks/pages/_error-6e6353f79838677c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
