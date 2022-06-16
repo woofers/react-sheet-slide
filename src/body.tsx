dd4fa4541a465ac49629135b4b445e70799898f9
Jaxson Van Doorn <woofers1tt@gmail.com>
2022-06-16 09:11:00 -0700

    Move class to body element

--- src/body.tsx
+++ src/body.tsx
@@ -1,10 +1,14 @@
 import React, { forwardRef, useEffect } from 'react'
 import { animated } from '@react-spring/web'
 import { hasWindow, setRef } from './utils'
+import classes from './classnames'
+import styles from './sheet.module.css'
 
 const values = ['--dim', '--scale', '--down', '--round', '--scroll']
 
 type BodyProxyProps = { style?: Record<string, string> }
+
+const cx = classes.bind(styles)
 
 const BodyProxy = forwardRef<HTMLBodyElement, BodyProxyProps>((_, ref) => {
   useEffect(() => {
@@ -12,10 +16,13 @@ const BodyProxy = forwardRef<HTMLBodyElement, BodyProxyProps>((_, ref) => {
     const elem = document.body as HTMLBodyElement
     const scroll = window.pageYOffset ?? 0
     elem.style.setProperty('--scroll', `${scroll}px`)
+    const className = cx('sheet-open')
+    elem.classList.add(className)
     setRef(ref, elem)
     return () => {
       if (!elem) return
       values.forEach(value => elem.style.removeProperty(value))
+      elem.classList.remove(className)
     }
   }, [ref])
   return null
